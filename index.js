1303d97b7ee805674989bb31974a7a8043d330e9
Vansh Kapoor <vanshkapoor2001@gmail.com>
2020-08-28 12:14:02 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-// const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,25 +10,27 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var inputNumber ;
+var inputNumber;
 var colors = []
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.render("home");
 });
-app.post("/",function(req,res){
+app.post("/", function(req, res) {
   const a = req.body;
   inputNumber = Math.floor(a.inputN);
-  colors.push(a.color1,a.color2,a.color3);
+  colors.push(a.color1, a.color2, a.color3);
   res.redirect("/grid");
 });
-app.get("/grid",function(req,res){
-  res.render("grid", {noOfGrid : inputNumber,colorPref : colors});
+app.get("/grid", function(req, res) {
+  res.render("grid", {
+    noOfGrid: inputNumber,
+    colorPref: colors
+  });
   colors = [];
 });
 
 
-
-app.listen( process.env.PORT || "3000", function() {
+app.listen(process.env.PORT || "3000", function() {
   console.log("Server has started sucessfully");
 });
